b7f8acdc643ce1c96f53f97c09db0e32beb87774
lrvick <lance@lrvick.net>
2012-01-15 18:23:48 -0500

    solved problem 5

---
+++ problem5.py
@@ -0,0 +1,23 @@
+# 2520 s the smallest number that can be divided by each of the numbers from 1
+# to 10 without any remainder.
+
+# What is the smallest positive number that is evenly divisible by all of the
+# numbers from 1 to 20?
+
+i = 1
+c = 20
+for x in range(c,1,-1):
+    for y in range(2,(x/2)+1):
+        if x%y==0:
+            break
+    else:
+        i *= x
+s = i
+while True:
+    for x in range(c,1,-1):
+        if s%x != 0:
+            break
+    else:
+        print s
+        break
+    s += i
