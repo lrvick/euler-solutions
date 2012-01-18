f02ef62021fa6b46de19689a1e5ce23d9846663d
lrvick <lance@lrvick.net>
2012-01-18 03:03:22 -0500

    slow solution for 9

---
+++ 9.py
@@ -0,0 +1,16 @@
+# A Pythagorean triplet is a set of three natural numbers, a  b  c, for which,
+
+# a2 + b2 = c2
+# For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
+#31.6227766017**2
+
+import itertools
+
+for a,b,c in itertools.combinations(range(1000),3):
+    if a+b+c == 1000 and a**2 + b**2 == c**2:
+        print a*b*c
+        break
