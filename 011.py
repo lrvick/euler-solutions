3a89bbba6c02e606cf16ebdce2f3f998dac61a3a
lrvick <lance@lrvick.net>
2012-01-19 15:20:07 -0500

    switch to map() for grid variable

--- 011.py
+++ 011.py
@@ -51,7 +51,7 @@ raw_grid = '\
 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 \
 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48'
 
-grid = [int(x) for x in raw_grid.split(' ')]
+grid = map(int,raw_grid.split(' '))
 
 size = 20
 
